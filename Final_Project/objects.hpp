5eba1ecd3b1fefe3a90cdb1070eb09e09d339c1f
PotatoFurr <potatofurr123@gmail.com>
2026-04-22 10:52:08 -0400

    //added objects.hpp and structure.hpp

---
+++ Final_Project/objects.hpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "VMIGameInterface.hpp"
+#include <VertexSets.hpp>
+#include <Vector3d.hpp>
+#include <Movement.hpp>
+#include <Thing.hpp>
+#include <MovingThing.hpp>
+#include <Color.hpp>
+
+//includes things like person model and obstacle models
+
+class Bird : public vmi::MovingThing
+{
+public:
+    Bird();
+    ~Bird();
+
+    void handleCollision(const vmi::Thing* other);
+
+    void move(double dt);
+
+    void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+
+    int getScore() const;
+
+private:
+    int score;              // how many pipes we've passed
+    vmi::Text scoreText;    // display of current score
+
+    void scorePoint();
+};
+
---
+++ Final_Project/structure.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "VMIGameInterface.hpp"
+#include <VertexSets.hpp>
+#include <Vector3d.hpp>
+#include <Movement.hpp>
+#include <Thing.hpp>
+#include <MovingThing.hpp>
+#include <Color.hpp>
+
+//includes things like the sections and wall destructor
